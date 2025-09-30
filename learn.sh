e750b41deff3210993d48a5a0ff1be0166904534
Katleho R Makhoba <katlehormakhoba@gmail.com>
2025-09-30 21:35:08 +0200

    chatper2

--- learn.sh
+++ learn.sh
@@ -2,19 +2,57 @@
 
 #-------------------------------CHAPTER 1 Variables and Basic Input/Output.
 
-my_name=Katleho
-age=55
+# my_name=Katleho
+# age=55
 
-ECHO "My name is $my_name and I am $age years old.😊"
-
-
-READ -P "Please enter you favorite color: " color
-
-ECHO "Your favorite color is: $color"
-
-#-------------------------------CHAPTER 2
+# echo "My name is $my_name and I am $age years old.😊"
 
 
-#-------------------------------CHAPTER 3
+# read -p "Please enter you favorite color: " color
+
+# echo "Your favorite color is: $color"
+
+
+# echo "Script name: $0"
+# echo "First arg: $1"
+# echo "Second arg: $2"
+# echo "Total args: $#"
+
+
+#-------------------------------CHAPTER 2 If/Else Conditions
+
+
+num=10
+
+
+if [ $num -ge 5 ]; then
+    echo "Number is greater than 5"
+fi
+
+age=$num
+
+if [ $age -ge 18 ]; then
+    echo "You are an Adult.😭"
+else
+    echo "You are still young🥳."
+fi
+
+# this is for your between
+#elif [ "$age" -ge 13 ] && [ "$age" -le 19 ]; then
+
+read -p "Enter traffic light color: " color
+
+if [ $color = "red" ]; then
+    echo "STOP"
+elif [ $color = "yellow" ]; then
+    echo "SLOW DOWN"
+else
+    echo "GO"
+fi
+
+
+#-------------------------------CHAPTER 3 Loops
+
+
 
 #-------------------------------CHAPTER 4
