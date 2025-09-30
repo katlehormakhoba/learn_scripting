396a5acb920518328c3e2c815637b3b284b08da3
Katleho R Makhoba <katlehormakhoba@gmail.com>
2025-09-30 20:41:26 +0200

    chapter 1

---
+++ learn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#-------------------------------CHAPTER 1 Variables and Basic Input/Output.
+
+my_name=Katleho
+age=55
+
+ECHO "My name is $my_name and I am $age years old.😊"
+
+
+READ -P "Please enter you favorite color: " color
+
+ECHO "Your favorite color is: $color"
+
+#-------------------------------CHAPTER 2
+
+
+#-------------------------------CHAPTER 3
+
+#-------------------------------CHAPTER 4
